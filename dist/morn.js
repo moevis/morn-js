1833566c02be9a6840b96180739343d538d71575
moe <icymorn@gmail.com>
2014-07-26 23:06:28 +0800

    data functions

--- dist/morn.js
+++ dist/morn.js
@@ -401,22 +401,108 @@ var morn = (function(){
 
 (function ($) {
 	$.browser = (function(){
-		var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+		var ua = navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
 		if(/trident/i.test(M[1])){
-			tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-			return 'IE '+(tem[1]||'');
-		}   
-	    if(M[1]==='Chrome'){
-			tem=ua.match(/\bOPR\/(\d+)/)
-			if(tem!=null) {
+			tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+			return 'IE ' + (tem[1] || '');
+		}
+		if(M[1] === 'Chrome'){
+			tem=ua.match(/\bOPR\/(\d+)/);
+			if(tem !== null) {
 				return 'Opera '+tem[1];
 			}
-		}   
-	    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-	    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-	    return {
-	    	browser:M[0], version: M[1]
-	    };
+		}
+		M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+		if((tem = ua.match(/version\/(\d+)/i)) !== null) {M.splice(1,1,tem[1]);}
+		return {
+			browser:M[0], version: M[1]
+		};
+	}());
+}(morn));
+'use strict';
+
+(function($) {
+	var cacheData = {
+
+	};
+
+	var guid = 0,
+		cache = 'morn-js';
+
+	$.prototype.data = function(key, value) {
+		if (value === undefined) {
+			if (this.dom[0][cache] === undefined){
+				return undefined;
+			} else {
+				var tmp = cacheData[this.dom[0][cache]];
+				if (tmp !== undefined) {
+					return tmp[key];
+				} else {
+					return undefined;
+				}
+			}
+		} else {
+			for (var i = 0, len = this.dom.length; i < len; i++) {
+				if (this.dom[i][cache] === undefined){
+					cacheData[guid] = {};
+					cacheData[guid][key] = value;
+					this.dom[i][cache] = guid++;
+				} else {
+					cacheData[this.dom[i][cache]][key] = value;
+				}
+			}
+			return this;
+		}
+	};
+
+	$.prototype.removeData = (function() {
+		try {
+			delete cacheData[''];
+			return function(key) {
+				var i, len;
+				if (key === undefined) {
+					for (i = 0, len = this.dom.length; i < len; i++) {
+						if (this.dom[i][cache] !== undefined){
+							delete cacheData[this.dom[i][cache]];
+						}
+					}
+				} else {
+					var tmp;
+					for (i = 0, len = this.dom.length; i < len; i++) {
+						if (this.dom[i][cache] !== undefined){
+							tmp = cacheData[this.dom[i][cache]];
+							if (tmp !== undefined) {
+								delete cacheData[this.dom[i][cache]];
+							}
+							delete this.dom[i][cache];
+						}
+					}
+				}
+
+			};
+		} catch(e) {
+			return function(key) {
+				var i, len;
+				if (key === undefined) {
+					for (i = 0, len = this.dom.length; i < len; i++) {
+						if (this.dom[i][cache] !== undefined){
+							cacheData.removeAttribute(this.dom[i][cache]);
+						}
+					}
+				} else {
+					var tmp;
+					for (i = 0, len = this.dom.length; i < len; i++) {
+						if (this.dom[i][cache] !== undefined){
+							tmp = cacheData[this.dom[i][cache]];
+							if (tmp !== undefined) {
+								cacheData.removeAttribute(this.dom[i][cache]);
+							}
+							this.dom[i].removeAttribute(cache);
+						}
+					}
+				}
+			};
+		}
 	}());
 }(morn));
 'use strict';
@@ -784,20 +870,20 @@ var morn = (function(){
 				case Token.TAG:
 					if (lastResult !== null) {
 						result = [];
-						if (tokens[i - 1].type !== Token.WHITE) {
-							for (var iter = 0, resultlen = lastResult.length; iter < resultlen; iter++) {
-								if (lastResult[iter].tagName === tokens[i - 1].text) {
-									result.push(lastResult[iter]);
-								}
-							}
-						} else {
+						// if (tokens[i - 1].type !== Token.WHITE) {
+						// 	for (var iter = 0, resultlen = lastResult.length; iter < resultlen; iter++) {
+						// 		if (lastResult[iter].tagName === tokens[i - 1].text) {
+						// 			result.push(lastResult[iter]);
+						// 		}
+						// 	}
+						// } else {
 							for (var iter = 0, resultlen = lastResult.length; iter < resultlen; iter++) {
 								var tmp = $.tag(tokens[i].text, lastResult[iter]);
 								for (var index = 0, l = tmp.length; index < l; index++) {
 									result.push(tmp[index]);
 								}
 							}
-						}
+						// }
 
 					} else {
 						result = $.tag(tokens[i].text);
@@ -826,10 +912,10 @@ var morn = (function(){
 								}
 							}
 
-						} else if (fake === 'first') {
+						} else if (fake === 'first-child') {
 								result = [lastResult[0]];
 
-						} else if (fake === 'last') {
+						} else if (fake === 'last-child') {
 								result = [lastResult[lastResult.length - 1]];
 						}
 					} else {
