9b803852a276a9c8887abb74921d40b981776c65
moe <icymorn@gmail.com>
2014-08-03 23:42:10 +0800

    ajax method, post and get

--- dist/morn.js
+++ dist/morn.js
@@ -76,6 +76,66 @@ var define = (function(){
 	};
 }());
 'use strict';
+
+define('ajax', ['core', 'promise'], function($){
+	function processData(data) {
+		var result = [];
+		for (var key in data) {
+			result.push(key + '=' + data[key]);
+		}
+		return result.join('&');
+	}
+
+	$.get = function(url, success, failed) {
+		var promise = $.promise(),
+			ajax = new XMLHttpRequest();
+
+		ajax.onload = function() {
+			if (ajax.status === 200) {
+				success(ajax.response);
+				promise.resolve(ajax.response);
+			} else {
+				if (failed) {
+					failed(ajax.statusText);
+				}
+				promise.reject(ajax.statusText);
+			}
+		};
+
+		ajax.open('GET', url, true);
+		ajax.send();
+
+		return promise;
+	};
+
+	$.post = function(url, data, success, failed) {
+		var promise = $.promise(),
+			ajax = new XMLHttpRequest(),
+			params = processData(data);
+
+		ajax.onload = function() {
+			if (ajax.status === 200) {
+				success(ajax.response);
+				promise.resolve(ajax.response);
+			} else {
+				if (failed) {
+					failed(ajax.statusText);
+				}
+				promise.reject(ajax.statusText);
+			}
+		};
+
+		ajax.open('POST', url, true);
+		
+		ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+		ajax.send(params);
+
+		return promise;
+	};
+
+});
+'use strict';
 /** 
  * This page of code is from the page: http://stackoverflow.com/questions/5916900/detect-version-of-browser
  * To get browser and version
