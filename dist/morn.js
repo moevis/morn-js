1d31f70684d2985ba8ead3ed9d4e1dee0a17a7db
moe <icymorn@gmail.com>
2014-08-05 00:09:19 +0800

    addAnimate

--- dist/morn.js
+++ dist/morn.js
@@ -1,4 +1,4 @@
-/*! morn-js - v0.0.1 - 2014-08-03 */
+/*! morn-js - v0.0.1 - 2014-08-05 */
 'use strict';
 
 var define = (function(){
@@ -134,6 +134,41 @@ define('ajax', ['core', 'promise'], function($){
 		return promise;
 	};
 
+});
+'use strict';
+
+define('animate', ['core', 'promise', 'dom'], function($) {
+	var queque = [];
+
+	$.tween = {
+		linear: function(k) {
+			return k;
+		}
+	};
+
+	$.prototype.addAnimate = function(attri, value, time, easing) {
+		var promise = $.promise(),
+			origin = $.getComputedStyle(this.dom[0], attri),
+			element = this.dom[0],
+			eclapse = 0,
+			startTime = Date.now(),
+			startValue = parseFloat(origin),
+			endValue = parseFloat(value);
+	
+		easing = easing || $.tween.linear;
+
+		var timer = setInterval(function(){
+				eclapse = Date.now() - startTime;
+				if (eclapse >= time) {
+					element.style[attri] = value;
+					promise.resolve();
+					clearInterval(timer);
+				} else {
+					element.style[attri] = startValue + easing(eclapse / time) * (endValue - startValue) + 'px';
+				}
+			}, 17);
+		return promise;
+	}
 });
 'use strict';
 /** 
